70b4942991e12789fbbd27ae3c9dc75a9c49687a
cchelton <59346941+cchelton@users.noreply.github.com>
2020-03-13 18:27:42 -0500

    Add Box Working

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,32 @@
+const employees = [];
+
 $(document).ready(init);
 
-function init() {}
+function init() {
+  $(".employeeDetailsForm").on("submit", addEmployee);
+  $(".theTable").on("click", ".js-delBtn", delEmployee);
+}
+
+function addEmployee(event) {
+  event.preventDefault();
+  const newEmployee = {
+    firstName: $(".js-input-fName").val(),
+    lastName: $(".js-input-lName").val(),
+    ID: $(".js-input-ID").val(),
+    title: $(".js-input-title").val(),
+    salary: $(".js-input-salary").val()
+  };
+  employees.push(newEmployee);
+  render();
+}
+
+function delEmployee() {
+  // TODO: delete employee
+}
+
+function render() {
+  $(".js-emptyThese").empty();
+  for (let i; i < employees.length; i++) {
+    //
+  }
+}
